399dd19c42cb47c3598d5c6854dd39f48f68a839
Dmitry Petruk <bavaria95@gmail.com>
2015-10-10 18:58:16 +0200

    parser of dates, which takes into account correctness of dates(with regard to days in months)

--- petruk.py
+++ petruk.py
@@ -26,6 +26,33 @@ def get_keywords(content):
     r = re.compile(pattern)
     return filter(None, r.findall(content)) # filter to remove empty keywords
 
+def get_number_of_diff_dates(content):
+    '''
+    dates can be in dd-mm-rrrr, dd/mm/rrrr, dd.mm.rrrr, rrrr-dd-mm, 
+    rrrr/dd/mm, rrrr.dd.mm formats. Also we need to guarantee that we
+    have used only distinct dates. 
+    '''
+    s = set() # set to store all dates in tuple: (dd, mm, yyyy)
+
+    # one type of date representation
+    pattern = r'((?P<year>\d{4})(?P<separ>[./-])(?P<date>(0[1-9]|[12][0-9]|3[01])(?P=separ)(0[13578]|1[02])|((0[1-9]|[12][0-9])(?P=separ)(02))|(0[1-9]|[12][0-9]|3[0])(?P=separ)(0[469]|11)))'
+    r = re.compile(pattern)
+    for x in r.finditer(content):
+        s.add(tuple(re.split(r'[./-]', x.group('date')) + [x.group('year')]))
+    # map(lambda x: s.add(tuple(re.split(r'[./-]', x.group('date')) + [x.group('year')])), r.finditer(content))
+
+    # another type of date representation(could be written with first one, but because of named groups the code is much simpler now)
+    pattern = r'(?P<full>((?P<date>(0[1-9]|[12][0-9]|3[01])[./-](0[13578]|1[02])|((0[1-9]|[12][0-9])[./-](02))|(0[1-9]|[12][0-9]|3[0])[./-](0[469]|11))[./-](?P<year>\d{4})))'
+    r = re.compile(pattern)
+    for x in r.finditer(content):
+        if re.match(r'\d{2}(?P<separ>[./-])\d{2}(?P=separ)\d{4}', x.group('full')):  # check if separator is the same
+            s.add(tuple(re.split(r'[./-]', x.group('date')) + [x.group('year')]))
+    # map(lambda x: s.add(tuple(re.split(r'[./-]', x.group('date')) + [x.group('year')])), r.finditer(content))
+    
+    return len(s)
+
+
+
 
 def processFile(filepath):
     fp = codecs.open(filepath, 'rU', 'iso-8859-2')
@@ -42,7 +69,7 @@ def processFile(filepath):
     print("liczba skrotow:")
     print("liczba liczb calkowitych z zakresu int:")
     print("liczba liczb zmiennoprzecinkowych:")
-    print("liczba dat:")
+    print("liczba dat: " + str(get_number_of_diff_dates(content)))
     print("liczba adresow email:")
     print("\n")
 
@@ -62,4 +89,5 @@ for root, dirs, files in tree:
         if f.endswith(".html"):
             filepath = os.path.join(root, f)
             processFile(filepath)
+            break
 
